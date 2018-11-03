ad8c258ca7452b3454886bfb0474b4673f586307
sonic-rectifier <galen.swyers@comcast.net>
2018-11-03 12:57:00 -0400

    Json commit

--- AngularJS/myCtrl.js
+++ AngularJS/myCtrl.js
@@ -2,7 +2,7 @@ app.controller("myCtrl", function($scope, $http) {
     $scope.firstName = "Matthew";
     $scope.lastName= "Penning";
 
-    $http.get("json_sample.html").then(function (response) {
+    $http.get("json.php").then(function (response) {
       $scope.myData = response.data.records;
     });
 });
