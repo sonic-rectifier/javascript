cad06f4c9f9b496f721624b1596d3e37ad439f2f
sonic-rectifier <galen.swyers@comcast.net>
2018-11-05 08:28:51 -0500

    fixed bug with myCtrl.js

--- AngularJS/myCtrl.js
+++ AngularJS/myCtrl.js
@@ -2,7 +2,7 @@ app.controller("myCtrl", function($scope, $http) {
     $scope.firstName = "Matthew";
     $scope.lastName= "Penning";
 
-    $http.get("jason.php").then(function (response) {
+    $http.get("json_sample2.html").then(function (response) {
       $scope.myData = response.data.records;
     });
 });
