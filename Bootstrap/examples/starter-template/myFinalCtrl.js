694548e2903dc179cd0d519563f57017f72d71ca
sonic-rectifier <galen.swyers@comcast.net>
2018-11-15 14:23:35 -0500

    push

--- Bootstrap/examples/starter-template/myFinalCtrl.js
+++ Bootstrap/examples/starter-template/myFinalCtrl.js
@@ -2,11 +2,16 @@ app.controller("myFinalCtrl", function($scope, $http) {
 
     $http.get("read.php").then(function (response) {
       $scope.myData = response.data.records;
-      foreach (data in $scope.myData){
+      // foreach (data in $scope.myData){
+      //   if (data.finished = "0"){
+      //     $scope.ToDoItems.push(data);
+      //   }
+      // }
+      $scope.myData.forEach(function(data){
         if (data.finished = "0"){
           $scope.ToDoItems.push(data);
         }
-      }
+      });
 
       //$scope.ToDoItems = $scope.myData;
 
