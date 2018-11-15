d4abcffee5aa7e979ea6d8c6f107a25c974462ca
sonic-rectifier <galen.swyers@comcast.net>
2018-11-15 14:32:49 -0500

    test

--- Bootstrap/examples/starter-template/myFinalCtrl.js
+++ Bootstrap/examples/starter-template/myFinalCtrl.js
@@ -8,9 +8,9 @@ app.controller("myFinalCtrl", function($scope, $http) {
       //   }
       // }
       for (var index = 0; index < $scope.myData.length; index ++  ){
-        if ($scope.myData.finished === 0){
+        //if ($scope.myData.finished === 0){
           $scope.ToDoItems.push($scope.myData[index]);
-        }
+        //}
       }
 
       //$scope.ToDoItems = $scope.myData;
