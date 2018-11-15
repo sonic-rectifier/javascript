5db11ec589861551b9c99131d1d708ae3dbb56ed
sonic-rectifier <galen.swyers@comcast.net>
2018-11-15 14:46:12 -0500

    reverted code, doing it in php now

--- Bootstrap/examples/starter-template/myFinalCtrl.js
+++ Bootstrap/examples/starter-template/myFinalCtrl.js
@@ -2,18 +2,7 @@ app.controller("myFinalCtrl", function($scope, $http) {
 
     $http.get("read.php").then(function (response) {
       $scope.myData = response.data.records;
-      // foreach (data in $scope.myData){
-      //   if (data.finished = "0"){
-      //     $scope.ToDoItems.push(data);
-      //   }
-      // }
-      for (var index = 0; index < $scope.myData.length; index ++  ){
-        //if ($scope.myData.finished === 0){
-          //$scope.ToDoItems[index] = index;
-        //}
-      }
 
-      $scope.ToDoItems = $scope.myData.item;
 
     });
 });
