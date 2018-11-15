119481c2b3e3220c6b1599b0ba24d18ef464e6c8
sonic-rectifier <galen.swyers@comcast.net>
2018-11-15 16:50:59 -0500

    added completed functionality

--- Bootstrap/examples/starter-template/myFinalCtrl.js
+++ Bootstrap/examples/starter-template/myFinalCtrl.js
@@ -2,7 +2,8 @@ app.controller("myFinalCtrl", function($scope, $http) {
 
     $http.get("readCompleted.php").then(function (response) {
       $scope.myData = response.data.records;
-
-
+    });
+    $http.get("read.php").then(function (response) {
+      $scope.myCompletedData = response.data.records;
     });
 });
