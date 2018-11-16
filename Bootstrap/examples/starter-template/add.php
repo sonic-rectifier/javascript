5b05c2c84d2ee15e87cfe0220006a96133b3bbbd
sonic-rectifier <galen.swyers@comcast.net>
2018-11-16 10:15:46 -0500

    first pass at add

---
+++ Bootstrap/examples/starter-template/add.php
@@ -0,0 +1,12 @@
+<?php
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type: application/json; charset=UTF-8");
+
+  $conn = new mysqli("localhost", "galen", "southhills#", "galen");
+
+  $result = $conn->query("INSERT INTO to_do_list (item, finished) VALUES ('".$data->item."', false)");
+
+  $conn->close();
+
+  echo($outp);
+?>
--- Bootstrap/examples/starter-template/myFinalCtrl.js
+++ Bootstrap/examples/starter-template/myFinalCtrl.js
@@ -7,4 +7,16 @@ app.controller("myFinalCtrl", function($scope, $http) {
     $http.get("readCompleted.php").then(function (response) {
       $scope.myCompletedData = response.data.records;
     });
+
+    $scope.add = function(){
+               $http({
+               method: 'post',
+               url: 'add.php',
+               data: {item:$scope.item_to_add_text},
+
+               }).then(function successCallback(response) {
+                   $scope.users.push(response.data[0]);
+               });
+           }
+
 });
