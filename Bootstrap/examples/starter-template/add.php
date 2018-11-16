a7229cd09df11d50fcf9994cfab5e046c601a7ef
sonic-rectifier <galen.swyers@comcast.net>
2018-11-16 10:24:36 -0500

    testing

--- Bootstrap/examples/starter-template/add.php
+++ Bootstrap/examples/starter-template/add.php
@@ -4,9 +4,9 @@
 
   $conn = new mysqli("localhost", "galen", "southhills#", "galen");
 
-//  echo "'.$data->item'.$data->item";
+  echo "'.$data->item'.$data->item";
 
-  $result = $conn->query("INSERT INTO to_do_list (item, finished) VALUES ('".$data->item."', false)");
+  //$result = $conn->query("INSERT INTO to_do_list (item, finished) VALUES ('".$data->item."', false)");
 
   $conn->close();
 
