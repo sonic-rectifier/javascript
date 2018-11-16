5c3980c4b375cdcfeaf65500fd367a9c671da76f
sonic-rectifier <galen.swyers@comcast.net>
2018-11-16 10:35:08 -0500

    test

--- Bootstrap/examples/starter-template/add.php
+++ Bootstrap/examples/starter-template/add.php
@@ -3,11 +3,12 @@
   header("Content-Type: application/json; charset=UTF-8");
 
   $conn = new mysqli("localhost", "galen", "southhills#", "galen");
-  $newItem = $data->item;
+  //$newItem = $data->item;
+  $newItem = "test2";
 //  echo "'.$data->item'.$data->item";
 
-  //$result = $conn->query("INSERT INTO to_do_list (item, finished) VALUES ('".$newItem."', false)");
-$result = $conn->query("INSERT INTO to_do_list (item, finished) VALUES ('test', false)");
+  $result = $conn->query("INSERT INTO to_do_list (item, finished) VALUES ('".$newItem."', false)");
+  //$result = $conn->query("INSERT INTO to_do_list (item, finished) VALUES ('test', false)");
   $conn->close();
 
   echo($outp);
