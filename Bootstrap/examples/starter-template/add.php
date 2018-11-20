c48d8236b6986a7b89ca9d33d92cd91d1dc522dd
sonic-rectifier <galen.swyers@comcast.net>
2018-11-20 08:22:23 -0500

    debugging

--- Bootstrap/examples/starter-template/add.php
+++ Bootstrap/examples/starter-template/add.php
@@ -2,9 +2,11 @@
   header("Access-Control-Allow-Origin: *");
   header("Content-Type: application/json; charset=UTF-8");
 
-  $newItem = $data->item;
   $conn = new mysqli("localhost", "galen", "southhills#", "galen");
 
+  $_POST = json_decode(file_get_contents('php://input'), true);
+
+  $newItem = $_POST['item'];
 
   $result = $conn->query("INSERT INTO to_do_list (item, finished) VALUES ('".$newItem."', false)");
   //$result = $conn->query("INSERT INTO to_do_list (item, finished) VALUES ('test', false)");
